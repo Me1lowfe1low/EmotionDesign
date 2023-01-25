736751747fd2a8f8b01947c6a009f86d8f76384c
C'est une pomme <dmgordienko@gmail.com>
2023-01-25 13:49:55 +0400

    Initial Commit

---
+++ EmotionDesign/EmotionDesignApp.swift
@@ -0,0 +1,17 @@
+//
+//  EmotionDesignApp.swift
+//  EmotionDesign
+//
+//  Created by Дмитрий Гордиенко on 25.01.2023.
+//
+
+import SwiftUI
+
+@main
+struct EmotionDesignApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
