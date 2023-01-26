3924bdee783199abac3d4c000bdb7a7a22903c78
C'est une pomme <dmgordienko@gmail.com>
2023-01-26 13:57:31 +0400

    This application would be created in order to check ones mental state that could be reviewed via graphs and plots in day, week, month, year period slices Per this commit following was performed: 1. 'ContentView.swift'		- added tab view for various functionality, such as: 1. adding information about your current feelings, 2. Informational tab, 3. Tab with all charts related to the current user 2. 'EmotionDesignApp.swift'	- added NavigationView to the code Models: 1. 'ColorMap.swift'			- added custom color to the assets of this project. This enum is required to use all custom colors 2. 'Emotion.swift'			- created struct that will hold all emotion types used in this application, additionally filled it with test data for debug purposes Views: 1. 'EmotionView.swift'		- view to represent each emotion. Added it also for testing purposes to check how gradient will look 2. 'EmotionShareView.swift'	- this view is a layer that gets the user to the view in which general emotions could be choosen 3. 'RoundShape.swift'		- created shapes for main emotions. RoundedShape would be the main disc. It was created scalable, so if any sections are required, segment could be added- working on improving this shape

---
+++ EmotionDesign/View/EmotionShareView.swift
@@ -0,0 +1,22 @@
+//
+//  EmotionShareView.swift
+//  EmotionDesign
+//
+//  Created by Дмитрий Гордиенко on 25.01.2023.
+//
+
+import SwiftUI
+
+struct EmotionShareView: View {
+    var body: some View {
+        NavigationLink(destination: RoundShape() ) {
+            Image(systemName: "plus")
+        }
+    }
+}
+
+struct EmotionShareView_Previews: PreviewProvider {
+    static var previews: some View {
+        EmotionShareView()
+    }
+}
