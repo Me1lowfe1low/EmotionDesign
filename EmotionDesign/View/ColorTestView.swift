5a737d7f348b12198ca41e7e6d83bcf5fb5a8b02
C'est une pomme <dmgordienko@gmail.com>
2023-02-28 15:42:31 +0400

    Removed file as it was created only for color testing: /Users/Ooops/Developer/EmotionDesign/EmotionDesign/View/ColorTestView.swift

---
+++ EmotionDesign/View/ColorTestView.swift
@@ -0,0 +1,20 @@
+// Created for EmotionDesign on 28.02.2023
+//  ColorTestView.swift
+//  EmotionDesign
+//
+//
+//    dmgordienko@gmail.com 2023
+
+import SwiftUI
+
+struct ColorTestView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct ColorTestView_Previews: PreviewProvider {
+    static var previews: some View {
+        ColorTestView()
+    }
+}
